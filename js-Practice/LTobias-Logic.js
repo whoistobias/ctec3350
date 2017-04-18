8219881522f13eff46d3f387c1a5ea3ee89088d0
whoistobias <whoistobias@gmail.com>
2017-04-18 16:56:03 -0500

    js practice added (also fixes for BEM Assignment

---
+++ js-Practice/LTobias-Logic.js
@@ -0,0 +1,18 @@
+/*
+   Creator: Tobias Lasco
+   Date: 04.18.2017
+   Name of File: LTobias-Logic.js
+   Description: Learning Operators, Comparisons, and Equalities
+   Website:
+   Version Number:
+   
+   !     NOT
+   &&    AND
+   ||    OR
+   
+*/
+
+var welcome = "Hey nerd! Come on in and get REKT!";
+alert(welcome);
+
+console.log();
